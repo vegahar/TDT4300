afa8945b21e392f4862305c98036d8748cc20bf3
Vegard Harper <vegahar@stud.ntnu.no>
2014-02-24 15:46:11 +0100

    completed fkminus1fkminus1

--- oving1/src/no/ntnu/idi/dm/arm/apriori/FkMinus1FKMinus1.java
+++ oving1/src/no/ntnu/idi/dm/arm/apriori/FkMinus1FKMinus1.java
@@ -22,8 +22,23 @@ public class FkMinus1FKMinus1<V> extends BaseApriori<V> {
 		for(ItemSet<V> itemSet1 : frequentCandidatesKMinus1){
 
     		for(ItemSet<V> itemSet2 : frequentCandidatesKMinus1){
+                /*if(itemSet1.equals(itemSet2)){
+                    continue;
+                } */
 
+                ItemSet<V> temp = new ItemSet<V>();
 
+                if(itemSet1.firstKItemsIdentical(itemSet2) == itemSet1.size()-1){
+                    temp = itemSet1.union(itemSet2);
+                }
+
+                if(temp.size() != itemSet1.size()+1){
+                    continue;
+                }
+
+                allGeneratedCandidatesCounter++;
+                getAndCacheSupportForItemset(temp);
+                frequentCandidateSet.add(temp);
             }
 
         }
